330607c60d47d8dba844cc3244514c51f265465d
Phung Van Tu <cube438126@gmail.com>
2024-11-12 22:36:08 +0700

    sua

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,5 +1,9 @@
 class SiteController {
-    
+    index(req, res){
+        res.json({
+            name: 'test'
+        })
+    }
     // get trang
     index(req, res){
         res.render('home');
