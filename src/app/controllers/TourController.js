77c737aebfb9ab32124c5ad8a5445fdf8c8199cc
PoppyPush <146519793+PoppyPush@users.noreply.github.com>
2024-11-19 10:05:39 +0700

    Create TourController.js
    Tìm Tour (chưa test)

---
+++ src/app/controllers/TourController.js
@@ -0,0 +1,21 @@
+const Tour=require('../model/Tour');
+
+class searchTourController{
+    async Search(req, res){
+        try{
+            const{name, level, slug}= req.query;
+            const query={};
+            if (name) query.name={ $regex: name, $option :'i'};
+            if (level) query.level=level;
+            if (slug) query.timestamp= slug;
+
+            const SeachTour= await Tour.find(query);
+            res.render('SeachTour',{SeachTour});
+        }catch(err){
+            console.error(err);
+            res.status(500).json({ message: "Seem to be nothing like you want", error: err });
+        }
+        
+    }
+}
+module.exports = new searchTourController();
