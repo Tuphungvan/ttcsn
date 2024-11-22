8a513d4ef784dc46e761a9a0cebe6910b062798a
PoppyPush <146519793+PoppyPush@users.noreply.github.com>
2024-11-22 17:39:30 +0700

    Update tour.js

--- src/routes/tour.js
+++ src/routes/tour.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
-const SearchTourController = require('../app/controllers/SearchTourController')
+const searchTourController = require('../app/controllers/SearchTourController')
 // Route tìm kiếm tours
-router.get('/tours/search', SearchTourController.search)
-router.get('/', SearchTourController.index)
-module.exports = router
+router.get('/tour', searchTourController.Search);
+router.get('/', searchTourController.index);
+module.exports = router;
