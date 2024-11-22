3e711f58f55d0f4eeda401026223e919e9376bb3
PoppyPush <146519793+PoppyPush@users.noreply.github.com>
2024-11-22 11:20:21 +0700

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,15 +3,16 @@ const siteRouter = require('./site');
 const authRouter = require('./auth');
 const profileRouter = require('./profile');
 const adminRouter = require('./admin');
-const SearchTourRouter = require('./tour');
-
+const SearchTourRouter = require('./tour/search');
+const OrderRouter = require('./order');
 function route(app) {
     app.use('/admin', adminRouter);
     app.use('/v1/auth', authRouter);
     app.use('/profile', profileRouter);
     app.use('/news', newsRouter);
     app.use('/', siteRouter);
-    app.use('/SearchTour', SearchTourRouter);
+    app.use('/tours/search', SearchTourRouter);
+    app.use('/order',OrderRouter);
 }
 
 module.exports = route;
