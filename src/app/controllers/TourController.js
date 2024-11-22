fbd7530baff28344dc35758d4c2748249a671556
PoppyPush <146519793+PoppyPush@users.noreply.github.com>
2024-11-22 17:41:17 +0700

    Update TourController.js

--- src/app/controllers/TourController.js
+++ src/app/controllers/TourController.js
@@ -5,12 +5,32 @@ class SearchTourController{
         try{
             const{name, level, slug}= req.query;
             const query={};
-            if (name) query.name={ $regex: name, $option :'i'};
+            if (name) query.name={ $regex: name, $options :'i'};
             if (level) query.level=level;
             if (slug) query.timestamp= slug;
 
-            const SearchTour= await Tour.find(query);
-            res.render('SearchTour',{SearchTour});
+            const searchTour= await Tour.find(query);
+            res.render('SearchTour',{searchTour});
+        }catch(err){
+            console.error(err);
+            res.status(500).json({ message: "Seem to be nothing like you want", error: err });
+        }
+        
+    }
+}
+module.exports = new SearchTourController();const Tour=require('../model/Tour');
+
+class SearchTourController{
+    async Search(req, res){
+        try{
+            const{name, level, slug}= req.query;
+            const query={};
+            if (name) query.name={ $regex: name, $options :'i'};
+            if (level) query.level=level;
+            if (slug) query.timestamp= slug;
+
+            const searchTour= await Tour.find(query);
+            res.render('SearchTour',{searchTour});
         }catch(err){
             console.error(err);
             res.status(500).json({ message: "Seem to be nothing like you want", error: err });
