a8390d8c29499d17c860e394b719c6809367d220
PoppyPush <146519793+PoppyPush@users.noreply.github.com>
2024-12-06 19:16:22 +0700

    Update TourController.js

--- src/app/controllers/TourController.js
+++ src/app/controllers/TourController.js
@@ -1,7 +1,7 @@
 const Tour=require('../models/Tour');
 
-class SearchTourController{
-    async Search(req, res){
+class TourController{
+    async search(req, res){
         try{
             const{name, level, slug}= req.query;
             const query={};
@@ -10,7 +10,7 @@ class SearchTourController{
             if (slug) query.timestamp= slug;
 
             const searchTour= await Tour.find(query);
-            res.render('SearchTour',{searchTour});
+            res.render('search',{searchTour});// Tên giao diện trong views
         }catch(err){
             console.error(err);
             res.status(500).json({ message: "Seem to be nothing like you want", error: err });
@@ -18,24 +18,4 @@ class SearchTourController{
         
     }
 }
-module.exports = new SearchTourController();const Tour=require('../model/Tour');
-
-class SearchTourController{
-    async Search(req, res){
-        try{
-            const{name, level, slug}= req.query;
-            const query={};
-            if (name) query.name={ $regex: name, $options :'i'};
-            if (level) query.level=level;
-            if (slug) query.timestamp= slug;
-
-            const searchTour= await Tour.find(query);
-            res.render('SearchTour',{searchTour});
-        }catch(err){
-            console.error(err);
-            res.status(500).json({ message: "Seem to be nothing like you want", error: err });
-        }
-        
-    }
-}
-module.exports = new SearchTourController();
+module.exports = new TourController();
